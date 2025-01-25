b6ea458c1823e3a29a119a7dd436f3c82434866f
Francis Okorie <92164567+Francis-Okorie@users.noreply.github.com>
2025-01-25 12:31:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ onAuthStateChanged(auth, (user) => {
         const emailName = document.querySelector(".email-id");
         emailName.innerHTML = user.email;
         showUpdateStorage();
-        showAchive();
     } else {
         currentUserUID = null;
         window.location.href = "index.html";
@@ -86,7 +85,6 @@ achiveContainerHeader.style.fontWeight="300";
 
 createBtn.addEventListener("click", handleTextArea);
 saveNote.addEventListener("click", saveClick);
-achiveBtn.addEventListener("click", archiveClick);
 
 function handleTextArea(){
     note.classList.toggle("hide");
@@ -122,30 +120,25 @@ function saveClick (){
         return;
     }
 
-    const saveClickData = {
-        header: saveNoteHeader || "Untitled", 
-        text: saveNoteText
+    const date = new Date;
+    const createdAt ={
+        day : date.getDate(),
+        month : date.getMonth(),
+        year : date.getFullYear(),
+        hour : date.getHours(),
+        minutes : date.getMinutes().toString().padStart(2, "0"),
     }
+
+    const saveClickData = {
+        header: saveNoteHeader || "Untitled",
+        text: saveNoteText,
+        createdAt,
+    };
 
     saveNoteToFirebase(saveClickData);
 }
 
-function archiveClick (){
-    const saveNoteHeader = inputBoxHeader.value;
-    const saveNoteText = noteText.value;
-    if (noteText.value === ""){
-        errorDisplay ();
-        return;
-    }
 
-    const archiveClickData = {
-        header: saveNoteHeader || "Untitled",
-        text: saveNoteText
-    }
-
-    archiveNoteToFirebase(archiveClickData);
-
-}
 
 function showUpdateStorage() {
     if (!currentUserUID) return;
@@ -153,87 +146,145 @@ function showUpdateStorage() {
     onValue(notesRef, (snapshot) => {
         store.innerHTML = "";
         snapshot.forEach((childSnapshot) => {
-            const note = childSnapshot.val();
-            createNoteUI(note, childSnapshot.key, "notes");
+            const noteKey = childSnapshot.key;
+            const noteData = childSnapshot.val();
+            if(noteData && noteKey){
+                createNoteUI(noteData, noteKey);
+            }
         });
     });
 }
 
-function showAchive() {
-    if (!currentUserUID) return;
-    const archivesRef = ref(db, `users/${currentUserUID}/archive`);
-    onValue(archivesRef, (snapshot) => {
-        achiveTextCon.innerHTML = "";
-        snapshot.forEach((childSnapshot) => {
-            const archive = childSnapshot.val();
-            console.log("Archiving item:", archive);
-            createNoteUI(archive, childSnapshot.key, "archive");
-        });
-        achieveValueLength()
-    });
-
-    
-}
 
 
-function createNoteUI(data, key, type){
+
+function createNoteUI(noteData, noteKey){
     const containerNote = document.createElement("div");
-    containerNote.setAttribute("class", type === "notes" ? "bordercontainer" : "bordercontainer2");
+    containerNote.setAttribute("class", "bordercontainer");
 
     const header = document.createElement("h1");
     header.setAttribute("class", "header-font")
-    header.innerHTML = data.header;
+    header.innerHTML = noteData.header;
 
     const text = document.createElement("p");
     text.setAttribute("class", "text-small");
-    text.innerHTML = data.text;
+    text.innerHTML = noteData.text;
+
+    const createdAt = noteData.createdAt || {};
+    const { day, month, year, hour, minutes } = createdAt;
+
+    const monthNames = [
+        "Jan", "Feb", "March", "April", "May", "June",
+        "July", "August", "Sept", "Oct", "Nov", "Dec"
+    ];
+
+    const formattedMonth = monthNames[month] || "Unknown";
+    
+    const dateContent = document.createElement("div");
+    dateContent.setAttribute("class", "date-content")
+
+    const dateContainer = document.createElement("p")
+    dateContainer.innerHTML = `${day} ${formattedMonth}, ${year}`
+
+    const timeContainer = document.createElement("p")
+    timeContainer.innerHTML = `${hour}:${minutes}`
 
     const deleteBtn = document.createElement("img");
     deleteBtn.setAttribute("class", "delete-btn");
     deleteBtn.src = "images/trash3-fill.svg";
     deleteBtn.addEventListener("click", () => {
         console.log("Delete button clicked");
-    remove(ref(db, `users/${currentUserUID}/${type}/${key}`));
+        const deletedHeader = header.innerHTML;
+        const deletedText = text.innerHTML;
+        
+        const deletedArchive = document.createElement("div");
+        deletedArchive.setAttribute("class", "bordercontainer2");
+
+        const deletedNoteData = {
+            header: deletedHeader,
+            text: deletedText,
+            
+        };
+
+        const archiveRef = ref(db, `users/${currentUserUID}/archives/${noteKey}`);
+        set(archiveRef, deletedNoteData)
+        .then(() => {
+            remove(ref(db, `users/${currentUserUID}/notes/${noteKey}`))
+        })
+        .then(() => {
+            containerNote.remove();
+        })
+        .catch((error) => {
+            console.error("Error moving to archive:", error);
+        });
+
+
+        const deletedH1 = document.createElement("h1")
+        const deletedp = document.createElement("p");
+        const deletedImage = document.createElement("img");
+        deletedImage.setAttribute("class", "delete-btn");
+        const createdAt = noteData.createdAt;
+        deletedImage.src = "images/trash3-fill.svg"
+
+        deletedH1.innerHTML = deletedHeader;
+        deletedp.innerHTML = deletedText;
+
+        
+        
+
+
+        deletedImage.addEventListener("click", () => {
+            remove(ref(db, `users/${currentUserUID}/archives/${noteKey}`))
+            .then(() => {
+                deletedArchive.remove()
+            })
+            .catch((error) => {
+                console.error("Error deleting archived note:", error);
+            })
+        });
+
+        deletedArchive.appendChild(deletedH1);
+        deletedArchive.appendChild(deletedp);
+        deletedArchive.appendChild(deletedImage);
+        achiveTextCon.appendChild(deletedArchive);
+
+    
+
+
+        
     });
+
+    dateContent.appendChild(dateContainer);
+    dateContent.appendChild(timeContainer);
 
     containerNote.appendChild(header);
     containerNote.appendChild(text);
     containerNote.appendChild(deleteBtn);
-
-    if(type === "notes"){
+    containerNote.appendChild(dateContent)
+    if(store){
         store.appendChild(containerNote);
-    }else if(type === "archive") {
-        achiveTextCon.appendChild(containerNote);
+    }else {
+        console.log("No Parent Element");
     }
+
+    
 }
+
+
+function deletedArchiveToFirebase(){}
+
+
 
 function saveNoteToFirebase(saveClickData){
     inputHeader.value = ""; 
     noteText.value = "";
-    
     if (!currentUserUID) return;
     const notesRef = ref(db, `users/${currentUserUID}/notes`);
     push(notesRef, saveClickData);
 
 }
 
-function archiveNoteToFirebase(archiveClickData){
-    
-    if (!currentUserUID) return;
 
-    const archivesRef = ref(db, `users/${currentUserUID}/archive`);
-    push(archivesRef, archiveClickData)
-        .then(() => {
-            console.log("Note archived successfully!");
-            showAchive();  
-        })
-        .catch((error) => {
-            console.error("Error archiving note:", error);
-    });
-
-    inputHeader.value = ""; 
-    noteText.value = "";
-}
 
 function errorDisplay () {
     errorText.classList.remove("hide");
@@ -243,5 +294,31 @@ function errorDisplay () {
     }, 3000);
 }
 
+const speechBtn = document.querySelector(".speech-btn");
+const speech = true;
+window.SpeechRecognition = window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.continuous = true; 
+let isRecording = false;
+
+recognition.addEventListener("result", (e) => {
+    const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("");
+    noteText.innerHTML = transcript;
+});
+
+speechBtn.addEventListener("click", ()=> {
+    
+    if(isRecording){
+        recognition.stop();
+        isRecording = false;
+    }else {
+        recognition.start();
+        isRecording = true;
+    }
+});
+
 showUpdateStorage();
-showAchive();
